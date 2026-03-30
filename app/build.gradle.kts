c1ff044268f04a84600ab4e804c70de9bc6bf21e
tomatojuice <tomato.juice.7@gmail.com>
2026-03-31 05:33:42 +0900

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ratolab.skin.calculator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.3"
+        versionCode = 13
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
